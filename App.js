033519f82fca9f0807d72569bf8865182f4b4d32
Rahul <rahul.s@neudesic.com>
2024-05-30 15:56:38 +0530

    Lecture: 03

--- App.js
+++ App.js
@@ -30,8 +30,11 @@ Ques: Create below HTML structure using React.
 Note: In real applications, HTML structures are very complex & creating using React (as above) will be a complex task.
 */
 
+//Lecture: 02
+
+/*
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child1" }, [
@@ -52,4 +55,67 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // render() method is responsible to convert the JS object into HTML element
 root.render(parent);
 
+*/
+
+//Lecture: 03
+
+/*
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {id: "heading"}, "This is a h1 heading");
+
 //TO AVOID CREATING HTML TAGS/ ELEMENTS USING "React.createElement", JSX COMES IN PICTURE !!!
+
+//JSX is not HTML in JS, it is HTML like syntax, using which we can create HTML tags/ elements easily. Also, JSX is different & React is different.
+
+//This is not pure JS, JS engines understand JS (es6 or es). But, JS engine still understands that's bcoz of bundler (parcel).
+//JSX => transpiled before it reaches to JS engine. "parcel" uses "babel" to do it.
+//JSX => React.createElement (babel does it) => React element or JS Object => render => HTML
+const jsxHeading = <h1 id="heading">This is a h1 heading using JSX.</h1>;
+
+//"jsxHeading" is also a JS object, same to "heading".
+// console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
+
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// FunctionalComponent: It's a normal JS function which returns JSX.
+
+const FunctionalComponent = function() {
+    return (
+        <div is="container">
+            <h1>This is a FunctionalComponent.</h1>
+        </div>
+    )
+}
+
+//We can call another component inside a component as well. 
+//Using "{}" inside JSX, we can write any JS. In JSX, using "{}" is very safe, it sanities the api response & saves from misc attacks.
+//We can add React elements in a components.
+
+const data = 1000;
+
+const header = React.createElement("h2", {id: "head"}, "This is a h2 header.");
+
+const FunctionalComponent2 = function() {
+    return (
+        <div is="container">
+            <FunctionalComponent />
+            {FunctionalComponent()}
+            {data}
+            {header}
+            <h1>This is FunctionalComponent2.</h1>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<FunctionalComponent2 />);
